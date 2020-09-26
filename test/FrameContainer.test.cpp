399cc4864bcaf6e02405111a1f701fec83885f3f
Ansu Singh <ansumsingh@gmail.com>
2020-09-27 09:13:00 +1000

    added more constructor tests for FrameContainer

--- test/FrameContainer.test.cpp
+++ test/FrameContainer.test.cpp
@@ -1,11 +1,41 @@
 #include "FrameContainer.h"
-#include "ffmpeg.h"
+#include "FFmpegException.h"
 #include <catch2/catch.hpp>
+
+namespace {
+	auto dummyAvFrame = []() {
+		auto frame = av_frame_alloc();
+		frame->width = 1092;
+		frame->height = 1080;
+		frame->format = AVPixelFormat::AV_PIX_FMT_BGR4;
+		REQUIRE(av_frame_get_buffer(frame, 0) == 0);
+
+		return frame;
+	};
+}
+
+namespace ffmpegcpp::unittests {
 
 TEST_CASE("FrameContainer()")
 {
-//   REQUIRE_NOTHROW([](){
-//   auto container = ffmpegcpp::FrameContainer{new AVFrame, new AVRational};
-//   });
-  auto container = ffmpegcpp::FrameContainer{new AVFrame, new AVRational};
+  REQUIRE_THROWS_AS(ffmpegcpp::FrameContainer(new AVFrame{}, new AVRational{}), ffmpegcpp::FFmpegException);
+
+  SECTION("Construct without throw")
+  {	  
+	  auto frame = dummyAvFrame();
+	  REQUIRE_NOTHROW(ffmpegcpp::FrameContainer(frame, new AVRational{}));
+  }
 }
+
+TEST_CASE("FrameContainer::getFrame()")
+{
+	auto frame = dummyAvFrame();
+	auto frameContainer = ffmpegcpp::FrameContainer{ frame, new AVRational{} };
+	auto avFrame = frameContainer.GetFrame();
+	
+	REQUIRE(avFrame->width == 1092);
+	REQUIRE(avFrame->height == 1080);
+	REQUIRE(avFrame->format == AVPixelFormat::AV_PIX_FMT_BGR4);
+}
+
+}//ffmpegcpp::unittests
