47730a6b2c0e6e4a55645f0b1e263509aff6da8c
Ansu Singh <ansumsingh@gmail.com>
2020-12-28 10:38:02 +1100

    clean-up unittest using ctest in local machine

--- build-libraries.sh
+++ build-libraries.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 BUILD_EXAMPLES_FLAG=true
 mkdir build
 
@@ -12,5 +11,7 @@ echo BUILD_EXAMPLES_FLAG="${BUILD_EXAMPLES_FLAG}"
 cmake -s . -B build -DBUILD_EXAMPLES=${BUILD_EXAMPLES_FLAG} -DCMAKE_TOOLCHAIN_FILE=${PWD}/dependencies/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug
 
 cmake --build build
-cd build
+
+pushd build
 ctest -V
+popd
